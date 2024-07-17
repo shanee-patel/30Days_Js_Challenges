4b010c3a89f3c15d4e1dc88f4b20462de2a9d296
Shanee Patel <shaneepatel3@gmail.com>
2024-07-17 12:17:41 +0530

    Day1 Task Completed

---
+++ Day_01/Script.js
@@ -0,0 +1,44 @@
+//Activity 1: variable Declaration
+//Task number 1
+var number = 9
+console.log(number);
+
+//Task number 2
+let Name = "shanee"
+console.log(Name);
+
+//Activity 2 Constant Declaration
+//task number 3
+const value = true
+console.log(value);
+
+//Activity 3 Data Types
+//task number 4
+console.log(number, typeof number);
+console.log(Name, typeof Name);
+console.log(value,typeof value);
+
+let items={
+    "pen": true,
+    "price":10
+}
+console.log(items,typeof items)
+
+let array =[1,2,3,4,5]
+console.log(array, typeof array);
+
+
+//Activity 4: Reassiging Variables
+//Task number 5
+
+let price =15
+console.log(price);
+price=20
+console.log(price);
+
+//Activity 5: Understanding const
+//Task number 6
+const days =15
+console.log(days);
+days=20
+console.log(days);
